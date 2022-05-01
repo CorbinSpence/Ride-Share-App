a6a05ef591707cad05faaebdd315125c79499fa4
KalybSanders <kalyb.sanders@gmail.com>
2022-05-01 11:50:57 -0400

    added profile button functionality.

--- app/src/main/java/edu/uga/cs/mobile_dev_final_project/Home_Page.java
+++ app/src/main/java/edu/uga/cs/mobile_dev_final_project/Home_Page.java
@@ -7,11 +7,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Home_Page extends AppCompatActivity implements View.OnClickListener{
     private RecyclerView rView;
     private ImageView car, seat;
+    private Button profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +23,14 @@ public class Home_Page extends AppCompatActivity implements View.OnClickListener
         rView = findViewById(R.id.driverView);
         car = findViewById(R.id.selectDriver);
         seat = findViewById(R.id.selectRider);
+        profile = findViewById(R.id.profile_button);
+
         DriversRecyclerView drv = new DriversRecyclerView(this);
         rView.setAdapter(drv);
         rView.setLayoutManager(new LinearLayoutManager(this));
         car.setOnClickListener(this);
         seat.setOnClickListener(this);
+        profile.setOnClickListener(this);
     }
 
     @Override
@@ -35,6 +41,9 @@ public class Home_Page extends AppCompatActivity implements View.OnClickListener
                 break;
             case R.id.selectRider:
                 startActivity(new Intent(this, RiderOptionsPage.class));
+                break;
+            case R.id.profile_button:
+                startActivity(new Intent(this, UserProfileActivity.class));
                 break;
         }
     }

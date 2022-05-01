36733f08e70882009ddb6b898d09b68524c5b111
CorbinSpence <46639741+CorbinSpence@users.noreply.github.com>
2022-05-01 12:54:16 -0400

    Added spinner. Spinner needs More functionality.

--- app/src/main/java/edu/uga/cs/mobile_dev_final_project/Home_Page.java
+++ app/src/main/java/edu/uga/cs/mobile_dev_final_project/Home_Page.java
@@ -1,19 +1,37 @@
 package edu.uga.cs.mobile_dev_final_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Home_Page extends AppCompatActivity implements View.OnClickListener{
     private RecyclerView rView;
     private ImageView car, seat;
     private Button profile;
+    private ArrayList<Post> list;
+    private final String TAG = "HomePage";
+    private FirebaseDatabase fd = FirebaseDatabase.getInstance();
+    private DatabaseReference dbr = fd.getReference("RequestData");
+    private Spinner spin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,14 @@ public class Home_Page extends AppCompatActivity implements View.OnClickListener
         car = findViewById(R.id.selectDriver);
         seat = findViewById(R.id.selectRider);
         profile = findViewById(R.id.profile_button);
+        spin = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> spinAdapter = ArrayAdapter.createFromResource(this, R.array.spinner, android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(spinAdapter);
+        spin.setOnItemSelectedListener(new MySpinner());
+
+        list = new ArrayList<Post>();
+        setListData();
 
         DriversRecyclerView drv = new DriversRecyclerView(this);
         rView.setAdapter(drv);
@@ -45,6 +71,63 @@ public class Home_Page extends AppCompatActivity implements View.OnClickListener
             case R.id.profile_button:
                 startActivity(new Intent(this, UserProfileActivity.class));
                 break;
+        }
+    }
+    private void setListData(){
+
+        //ArrayList<Post> al = new ArrayList<Post>();
+        dbr.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot dSnapshot : snapshot.getChildren()){
+
+                    Log.d(TAG, "Value added: "+dSnapshot.getValue(Post.class).toString());
+                    Log.d(TAG, "name of person: "+dSnapshot.getValue(Post.class).getPoster_name());
+                    list.add(dSnapshot.getValue(Post.class));
+                    show();
+                }
+                Log.d(TAG, "End of for loop.");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        // return al;
+    }
+    private void show() {
+        //RidersRecyclerView rrv = new RidersRecyclerView(this, list);
+
+        final RidersRecyclerView.MyClickListener mcl = new RidersRecyclerView.MyClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                if(position == 1) {
+                    startActivity(new Intent(Home_Page.this, Home_Page.class));
+                } else if(position == 2) {
+                    startActivity(new Intent(Home_Page.this, DriverOptionsPage.class));
+                }
+
+            }
+        };
+        RidersRecyclerView rrv = new RidersRecyclerView(this, list, mcl);
+
+        rView.setAdapter(rrv);
+        rView.setLayoutManager(new LinearLayoutManager(this));
+
+        //rv.setOnClickListener( this );
+    }
+    private class MySpinner implements AdapterView.OnItemSelectedListener {
+
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
         }
     }
 }
